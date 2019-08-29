001e3d065cbf14df377bc6a214d81dc377c0ce31
piavw <piavonwachenfelt@hotmail.com>
2019-08-29 15:11:09 +0200

    Refactoring code and creating a method.

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,12 +1,15 @@
-def fizz_buzz(number)  
-    if number % 15 == 0 then 'fizz buzz'
-    elsif number % 5 == 0 then 'buzz'
-    elsif number % 3 == 0 then 'fizz'
+def fizz_buzz(number)
+    if has_zero_remainder?(number,15) then 'fizz buzz'
+    elsif has_zero_remainder?(number,5) then 'buzz'
+    elsif has_zero_remainder?(number,3) then 'fizz'
     elsif number.is_a? String then 'please put in a number'
     else
-        number
+         number 
     end
-    
+end
+
+def has_zero_remainder?(number, divider)
+    number % divider == 0
 end
 
 
