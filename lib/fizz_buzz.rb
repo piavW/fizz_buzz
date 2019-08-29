c8f489441ad1c54a5ccb2c8643139c7ccf1e4212
piavw <piavonwachenfelt@hotmail.com>
2019-08-29 15:05:11 +0200

    Creating expectations for buzz and fizz buzz. Creating elsif statements and streamlining the code.

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,9 +1,11 @@
 def fizz_buzz(number)  
-    if number % 3 == 0
-        'fizz'
+    if number % 15 == 0 then 'fizz buzz'
+    elsif number % 5 == 0 then 'buzz'
+    elsif number % 3 == 0 then 'fizz'
     else
         number
     end
+    
 end
 
 
